0907a20a5564fd7c32e26f7b4ad34ba0306e69e4
adamSales <asales@utexas.edu>
2019-09-10 16:35:16 -0500

    multiple imputation pooling, interpreter saturation only for intepreter==1

--- regression.r
+++ regression.r
@@ -47,7 +47,7 @@ type <- c(type,rep('demo',ncol(dat)-length(type)))
 for(cc in cats){
     summ[[cc]] <- apply(dat[,type==cc],1,
       function(x) mean(x=='Extremely likely'|x=='Likely',na.rm=TRUE))
-    cont[[cc]] <- rowMeans(surv[,type==cc],na.rm=TRUE)
+    cont[[cc]] <- rowMeans(surv[,c(type[1],type[type%in%cats])==cc],na.rm=TRUE)
   for(i in 1:4)
       resp[[paste0(cc,'.',levsAb[i])]] <- apply(dat[,type==cc],1,
                                                 function(x) mean(x==levs[i],na.rm=TRUE))
@@ -83,8 +83,10 @@ ddd$age <- as.numeric(dat$Age) ## continuous
 regDat <- bind_cols(cont,ddd)
 ## add some more/simplify predictors
 
-regDat$white <- regDat$Ethnicity=='White, European'
+regDat$white <- startsWith(regDat$Ethnicity,'White')
+regDat$white[regDat$Ethnicity=='NA'] <- NA
 regDat$main <- regDat$MainstreamingHS=='mainstream only'
+regDat$main[regDat$MainstreamingHS=='NA'] <- NA
 
 ##
 hst <- data.frame(
@@ -109,18 +111,24 @@ nad <- select(regDat,deafDisabled,age,gender,`Interpreter Saturation`,white,deaf
 nad[nad=='NA'] <- NA
 nnn <- apply(nad,1,function(x) sum(is.na(x)))
 table(nnn)
-sapply(nad[which(nnn>4),],function(x) sum(is.na(x))) ## 7 or 8 means nearly all missing
-regDat <- regDat[nnn<5,]
+sapply(nad[which(nnn==7),],function(x) sum(is.na(x))) ##
+regDat <- regDat[nnn<8,]
+
+select(regDat,deafDisabled,age,gender,`Interpreter Saturation`,white,deafHS,deafHSprog,Interpreters,
+  `Speech-to-Text`,Notetaking,ExtendedTesttime)%>%
+  map_dbl(~sum(is.na(.)))
+
+cat(
+  ' ----------------------------------\n',
+  'dropping ',nrow(dat)-nrow(regDat),' people due to too much missingness\n',
+  '----------------------------------\n'
+)
 
 names(regDat) <- gsub(' |-','',names(regDat))
 
 impDat <- mice(regDat,m=20)
 
-imp1 <- complete(impDat)
 
-long <- gather(imp1,"scale","rating",tech:scap)
-
-names(long) <- gsub(' |-','',names(long))
 
 effByScale <- function(mod){
   re <- ranef(mod,condVar=TRUE)
@@ -154,15 +162,16 @@ byScaleCoef <- function(ebs){
   out
 }
 
-model1 <- function(varb){
+model1 <- function(varb,long){
   form <- rating~(1|id)
   form1 <- update(form,paste('.~.+(1|scale)+',varb))
   form2 <- update(form,paste('.~.+(',varb,'|scale)+',varb))
-  list(tp=lmer(form1,data=long,REML=FALSE),pp=lmer(form2,data=long,REML=FALSE))
+  LLL <- long
+  list(tp=lmer(form1,data=LLL,REML=FALSE),pp=lmer(form2,data=LLL,REML=FALSE))
 }
 
-proc1 <- function(varb){
-  models <- model1(varb)
+proc1 <- function(varb,long){
+  models <- model1(varb,long)
   effs <- effByScale(models[[2]])
   out <- rbind(summary(models[[1]])$coef,byScaleCoef(effs))
   attr(out,'models') <- models
@@ -171,18 +180,87 @@ proc1 <- function(varb){
 }
 
 
-long$`age/10` <- long$age/10
-long$InterpreterSaturation <- factor(long$InterpreterSaturation,ordered=FALSE,levels=c("low (0-50)",   "med (51-125)", "high (126+)" ))
-
-models <- sapply(
-  c('deafDisabled','InterpreterSaturation','`age/10`','white','gender',
+varbs <- c('deafDisabled',#'InterpreterSaturation',
+    '`age/10`','white','gender',
     'deafHS+deafHSprog',
-    'Interpreters+SpeechtoText+Notetaking+ExtendedTesttime'),
-  proc1,
-  simplify=FALSE)
+    'Interpreters+SpeechtoText+Notetaking+ExtendedTesttime')
 
-mods <- map(models,~attr(.,"models")[[1]])
-mods2 <- map(models,~attr(.,"models")[[2]])
+transMIdat <- function(impDat)
+  map(seq(impDat$m),
+    function(mm){
+      imp1 <- complete(impDat,mm)
+      long <- gather(imp1,"scale","rating",tech:scap)
+      names(long) <- gsub(' |-','',names(long))
+      long$`age/10` <- long$age/10
+      long$InterpreterSaturation <- factor(long$InterpreterSaturation,ordered=FALSE,levels=c("low (0-50)",   "med (51-125)", "high (126+)" ))
+      long
+    })
+
+
+
+miV <- function(vv,longDats){
+  m <- length(longDats)
+
+  mods <- map(longDats,~proc1(vv,.))
+  ests <- do.call('cbind',map(mods,~.[['Estimate']]))
+  ses <- do.call('cbind',map(mods,~.[['Std. Error']]))
+  Ubar <- rowMeans(ses^2)
+  B <- apply(ests,1,var)
+  if(all(B==0)) return(mods[[1]])
+  corr <- (1+1/impDat$m)
+  v0 <- mods[[1]][['df']]
+  vm <- ifelse(B==0,v0,(m-1)*(1+Ubar/(corr*B)))
+
+  out <- tibble(
+    Estimate= rowMeans(ests),
+    `Std. Error`=sqrt(Ubar+corr*B),
+    df=ifelse(is.na(v0),Inf,v0),
+    `t value`=Estimate/`Std. Error`,
+    `Pr(>|t|)`=2*pt(-abs(`t value`),df)
+  )
+  out <- as.data.frame(out)
+  rownames(out) <- rownames(mods[[1]])
+  attr(out,'components') <- map(mods,attributes)
+  out
+}
+
+mi <- function(varbs,impDat,subst=NULL){
+  longDats <- transMIdat(impDat)
+
+  if(!is.null(subst))
+    longDats <- lapply(longDats,function(x) x[eval(parse(text=subst),x),])
+
+  ovarbs <- sapply(sapply(varbs,strsplit,split='+',fixed=TRUE),str_extract,pattern='[:alpha:]+')
+  out <- list()
+  on.exit(cat('\n'))
+  for(vv in varbs){
+    cat(vv,' ')
+    out[[vv]] <-
+      if(all(impDat$nmis[ovarbs[[vv]]]==0)) proc1(vv,longDats[[1]]) else miV(vv,longDats)
+  }
+  out
+}
+
+
+
+res <- mi(varbs,impDat)
+
+intSat <- mi('InterpreterSaturation',impDat,subst="Interpreters==1")
+
+
+
+  ## out <- map(mods,function(mod){
+##     tibble(
+
+##     `Std. Error`= if(ncol(ebs$se)==2) ebs$se[,2] else do.call("c",ebs$se[,-1]),
+##     df=NA,
+##     `t value`=Estimate/`Std. Error`,
+##     `Pr(>|t|)`=2*pnorm(-abs(`t value`))
+##     )
+
+
+## mods <- map(models,~attr(.,"models")[[1]])
+## mods2 <- map(models,~attr(.,"models")[[2]])
 
 ### automatic model selection time
 
@@ -194,50 +272,50 @@ mods2 <- map(models,~attr(.,"models")[[2]])
 
 ## summary(modSelect <- step(modFull))
 
-modLessFull <- lmer(rating~(1|id)+deafDisabled+InterpreterSaturation+age+white+deafHS+deafHSprog+Interpreters+SpeechtoText+Notetaking+ExtendedTesttime+
- (age+white|scale),
-  data=long)
+## modLessFull <- lmer(rating~(1|id)+deafDisabled+InterpreterSaturation+age+white+deafHS+deafHSprog+Interpreters+SpeechtoText+Notetaking+ExtendedTesttime+
+##  (age+white|scale),
+##   data=long)
 
-summary(modSelect2 <- step(modLessFull))
+## summary(modSelect2 <- step(modLessFull))
 
-summary(selected <- update(modLessFull,rating ~ `age/10` + white + Notetaking + (1 | id) + (white | scale)))
+## summary(selected <- update(modLessFull,rating ~ `age/10` + white + Notetaking + (1 | id) + (white | scale)))
 
-mods$selected <- selected
-mods2$selected <- selected
-names(mods) <- NULL
-names(mods2) <- NULL
+## mods$selected <- selected
+## mods2$selected <- selected
+## names(mods) <- NULL
+## names(mods2) <- NULL
 
-htmlreg(mods,file='regressions1.html')
+## htmlreg(mods,file='regressions1.html')
 
 
-mod1a <- lmer(rating~(1|id)+(`Interpreter Saturation`|scale)+`Interpreter Saturation`,data=long)
+## mod1a <- lmer(rating~(1|id)+(`Interpreter Saturation`|scale)+`Interpreter Saturation`,data=long)
 
-mod2 <- lmer(rating~(1|id)+(1|scale)+age,data=long)
-mod2a <- lmer(rating~(1|id)+(age|scale)+age,data=long)
+## mod2 <- lmer(rating~(1|id)+(1|scale)+age,data=long)
+## mod2a <- lmer(rating~(1|id)+(age|scale)+age,data=long)
 
-mod3 <- lmer(rating~(1|id)+(1|scale)+white,data=long)
-mod3b <- lmer(rating~(1|id)+(white|scale)+white,data=long)
+## mod3 <- lmer(rating~(1|id)+(1|scale)+white,data=long)
+## mod3b <- lmer(rating~(1|id)+(white|scale)+white,data=long)
 
-#mod4 <-
+## #mod4 <-
 
-  ## c(
-      ##  'ruralUrban',
-      ##  'ruralUrbanIP',
-      ##  'Interpreter Saturation',
-      ##  'demo68',
-      ##  'demo67',
-      ##  'demo65',
-      ##  'Ethnicity',
-      ##  'Disability',
-      ##  'deafDisabled',
-      ##  'HStype',
-      ##  'MainstreamingHS',
-      ##  'age',
-      ##  'demo5',
-      ##  'gender',
-      ##  'demo26',
-      ##  'HS Class Language',
-      ##  'intSatCat',
-      ##  preferredLanguageVarbs
+##   ## c(
+##       ##  'ruralUrban',
+##       ##  'ruralUrbanIP',
+##       ##  'Interpreter Saturation',
+##       ##  'demo68',
+##       ##  'demo67',
+##       ##  'demo65',
+##       ##  'Ethnicity',
+##       ##  'Disability',
+##       ##  'deafDisabled',
+##       ##  'HStype',
+##       ##  'MainstreamingHS',
+##       ##  'age',
+##       ##  'demo5',
+##       ##  'gender',
+##       ##  'demo26',
+##       ##  'HS Class Language',
+##       ##  'intSatCat',
+##       ##  preferredLanguageVarbs
 
 
